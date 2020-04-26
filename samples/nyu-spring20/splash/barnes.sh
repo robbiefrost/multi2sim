242c943ad90e29a3755501e0c9fd10e4e3f012d4
Robert Frost <robbiefrost@gmail.com>
2020-04-26 00:29:53 -0400

    splach test script

--- samples/nyu-spring20/splash/barnes.sh
+++ samples/nyu-spring20/splash/barnes.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # For running on CIMS
-echo -e "Barnes Benchmark\n"
+# Make sure you've edited data-test/input for 4 cores
+# run with "2>&1 | tee log.txt" at the end to output to terminal and log.txt
+# start within tmux so you can detach from the session and leave it running
+echo -e "Barnes Benchmark"
 m2sExe=$1
 barnesPath=$2
 configPath=$3
@@ -10,8 +13,8 @@ declare -a policies=("lru"
 for pol in "${policies[@]}"
 do
     echo -e "\n$pol Policy"
-    $m2sPath --x86-sim detailed \
-     --x86-config $configPath/x86-config.ini \
-     --mem-config $configPath/mem-config-$pol.ini \
+    $m2sExe --x86-sim detailed \
+     --x86-config x86-config.ini \
+     --mem-config mem-config-$pol.ini \
      $barnesPath/barnes.i386 < $barnesPath/data-test/input
 done
