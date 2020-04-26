bc417f00cc51f2b8ccbe5b96fdb8a1b0669e4d40
Robert Frost <robbiefrost@gmail.com>
2020-04-25 20:20:04 -0400

    debugging

--- src/memory/Cache.cc
+++ src/memory/Cache.cc
@@ -208,7 +208,6 @@ void Cache::AccessBlock(unsigned set_id, unsigned way_id, int core_id)
 		set->lru_list.Erase(block->lru_node);
 		set->lru_list.PushFront(block->lru_node);
 	}
-
 	
 	if (replacement_policy == ReplacementLRUPartition) {
 		// Incerement core counter
@@ -224,8 +223,15 @@ void Cache::AccessBlock(unsigned set_id, unsigned way_id, int core_id)
 		} else {
 		// } else if (set->core_lru_list[core_id].getSize() > 1) {
 			std::cout << "access 3 core=" << core_id << " count=" << set->core_access_count[core_id] << "/" << set->core_access_count[num_cores];
-			std::cout << " list size=" << set->core_lru_list[core_id].getSize() << std::endl;
+			std::cout << " list size=" << set->core_lru_list[core_id].getSize();
+			if (block) {
+				std::cout << " block!" << std::endl;
+			} else {
+				std::cout << " no block :(" << std::endl;
+			}
+
 			set->core_lru_list[core_id].Erase(block->lru_node);
+			
 			std::cout << "access 3.5 core=" << core_id << " count=" << set->core_access_count[core_id] << "/" << set->core_access_count[num_cores] << std::endl;
 			set->core_lru_list[core_id].PushFront(block->lru_node);
 			std::cout << "access 4 core=" << core_id << " count=" << set->core_access_count[core_id] << "/" << set->core_access_count[num_cores] << std::endl;
